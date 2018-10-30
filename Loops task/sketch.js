0ce817cba4fbd926f167a2423c85b7f5401d0c2d
Claire Stranack <44362052+cstranack@users.noreply.github.com>
2018-10-30 17:39:19 +0000

    Add files via upload

---
+++ Loops task/sketch.js
@@ -0,0 +1,34 @@
+
+let size = 50;
+let size2 = 30
+let height = 0;
+function setup() {
+//test
+  frameRate(9);
+  createCanvas(500,500);
+
+  }
+//
+//loop in a loop
+  function draw() {
+    fill(255,200,200);
+    for(let i = 0; i <10; i++){
+      for(let x = 0; x < 10; x++){
+        rect(size*x, height, size, size);
+        //console.log("x: "+x+", height:" +height);
+    }
+    height = height + 50 }
+
+
+      //edit
+
+      for(let i = 0; i <10; i++){
+        for(let x = 0; x < 10; x++){
+          fill(random(255),random(255),random(255));
+          rect(size*x+10, size*i+10, size2, size2);
+
+          //console.log("x: "+x+", height:" +height);
+      }
+    }
+//test
+  }
