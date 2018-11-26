74ee1ddd220a95d38f0f7d7b71a1a9205935fd5a
Claire Stranack <44362052+cstranack@users.noreply.github.com>
2018-11-26 09:13:10 +0000

    Add files via upload
    update

--- DAT405/Coding practice document/sketch.js
+++ DAT405/Coding practice document/sketch.js
@@ -1,96 +1,58 @@
-
-var flag = "no";
-var mouseCoordX;
-var mouseCoordY;
-
 function setup(){
-    createCanvas(594,841);
-    fill(random(255),0,0,50);
-    frameRate(15);
-
+    createCanvas(1000,800);
+    //background(0,0,0);
 }
 
-var angle = 0;
-var angle2 = 0;
-var angle3 = 0;
-var angle4 = 0;
-var angle5 = 0;
-
-  function draw(){
-    drawCirles()
+function drawLines(){
+  for(i=0; i<1001; i = i + 20){
+    if (mouseIsPressed){
+      stroke(random(255),random(255),random(255));
+      line(0,i, mouseX, mouseY);
+      line(i,0, mouseX, mouseY);
+      line(i,1000, mouseX, mouseY);
+      line(1000,i, mouseX, mouseY);
+    }
+    else{
+      stroke(0,0,0);
+      line(0,i, mouseX, mouseY);
+      line(i,0, mouseX, mouseY);
+      line(i,1000, mouseX, mouseY);
+      line(1000,i, mouseX, mouseY);
   }
+  }
+}
 
-function drawCirles(){
-    if(angle<TWO_PI){
+function drawCentreCircle(){
+  fill(180);
+  noStroke();
+  ellipse(mouseX,mouseY,60,60);
+}
 
-      if(flag == "no"){
-        mouseCoordX = mouseX;
-        mouseCoordY = mouseY;
-        console.log(mouseCoordX);
-        console.log("yes");
-        flag = "yes";
-        }
-
-
-      else if(flag == "yes"){
-
-        stroke(180);
-        var negXCircleCoord = mouseCoordX - cos(angle2)*50;
-        var negYCircleCoord = mouseCoordY - sin(angle2)*50
-        var posXCircleCoord = mouseCoordX + cos(angle2)*50;
-        var posYCircleCoord = mouseCoordY + sin(angle2)*50;
-        var negXCircleCoord2 = mouseCoordX - cos(angle3)*100;
-        var negYCircleCoord2 = mouseCoordY - sin(angle3)*100;
-        var posXCircleCoord2 = mouseCoordX + cos(angle3)*100;
-        var posYCircleCoord2 = mouseCoordY + sin(angle3)*100;
-        var negXCircleCoord3 = mouseCoordX - cos(angle4)*150;
-        var negYCircleCoord3 = mouseCoordY - sin(angle4)*150;
-        var posXCircleCoord3 = mouseCoordX + cos(angle4)*150;
-        var posYCircleCoord3 = mouseCoordY + sin(angle4)*150;
-        var negXCircleCoord4 = mouseCoordX - cos(angle5)*200;
-        var negYCircleCoord4 = mouseCoordY - sin(angle5)*200;
-        var posXCircleCoord4 = mouseCoordX + cos(angle5)*200;
-        var posYCircleCoord4 = mouseCoordY + sin(angle5)*200;
+function drawStillCirles(){
+  var xCoord = mouseX + 30;
+  var yCoord = mouseY + 30;
+  var xCoord2 = mouseX - 30;
+  var yCoord2 = mouseY - 30;
+  var size = 20;
+  var fade = 100;
+  noStroke();
+  for(x = 0; x<1000; x = x+30){
+    fade = fade -5;
+    fill(240,250,0,fade);
+    ellipse(xCoord + x, yCoord + x,size,size);
+    fill(250,200,0,fade);
+    ellipse(xCoord2 - x, yCoord2 - x,size,size);
+    size = size + 5;
+  }
+}
 
 
-
-        background(255,255,255);
-        size = 50
-        ellipse(posXCircleCoord,posYCircleCoord,size,size);
-        ellipse(negXCircleCoord,negYCircleCoord,size,size);
-        ellipse(posXCircleCoord2,posYCircleCoord2,size,size);
-        ellipse(negXCircleCoord2,negYCircleCoord2,size,size);
-        ellipse(negXCircleCoord3,negYCircleCoord3,size,size);
-        ellipse(posXCircleCoord3,posYCircleCoord3,size,size);
-        ellipse(negXCircleCoord4,negYCircleCoord4,size,size);
-        ellipse(posXCircleCoord4,posYCircleCoord4,size,size);
+function draw(){
+  //background(180);
+  drawLines();
+  drawCentreCircle();
+  //drawStillCirles();
 
 
 
-        fill(random(255),random(255),random(255),20);
-        ellipse(posXCircleCoord,posYCircleCoord + 10,20,20);
-        ellipse(posXCircleCoord2,posYCircleCoord2 + 10,20,20);
-        ellipse(negXCircleCoord3 - 10,negYCircleCoord3 -10,20,20);
-        ellipse(posXCircleCoord3 - 10,posYCircleCoord3- 10,20,20);
-
-
-        angle = angle + 0.1;
-        angle2 = angle2 + 0.2;
-        angle3 = angle3 + 0.3;
-        angle4 = angle4 + 0.4;
-        angle5 = angle5 + 0.5;
-
-
-
-
-        }
-
-    }
-
-    else if(angle => TWO_PI){
-      background("clear");
-      angle = 0;
-      mouseCoordX = mouseX;
-      mouseCoordY = mouseY;
-    }
 }
--- DAT405/Creative coding Assignment 1/sketch.js
+++ DAT405/Creative coding Assignment 1/sketch.js
@@ -1,58 +1,134 @@
+
+// Declaring my variables
+//flag has a boolean value that will either be "yes" or "no"
+var flag = "no";
+//these variables have to be declared here but not assigned to a value
+var mouseCoordX;
+var mouseCoordY;
+// these variables determind at what degrees each of the cirles
+//turn around the centre point at each run of the program
+var angle = 0;
+var angle2 = 0;
+var angle3 = 0;
+var angle4 = 0;
+var angle5 = 0;
+
+//initialising the program
 function setup(){
-    createCanvas(1000,800);
-    //background(0,0,0);
+  //determining the canvas size, in pixels
+    createCanvas(594,841);
+  //All the shapes will be the same colour untill
+  //told otherwise
+    fill(random(255),0,0,50);
+  //set the frame rate determinds the rate of 'movement'
+    frameRate(15);
+}
+  //the draw fucntion will be repeated continuously
+  function draw(){
+    //the drawCirles function is being called here
+    drawCirles();
+    drawBorder();
+  }
+
+function drawBorder(){
+  stroke(100);
+  line(0,0,594,0);
+  line(0,0,0,841);
+  line(0,839,592,839);
+  line(592,0,592,839);
+
 }
 
-function drawLines(){
-  for(i=0; i<1001; i = i + 20){
-    if (mouseIsPressed){
-      stroke(random(255),random(255),random(255));
-      line(0,i, mouseX, mouseY);
-      line(i,0, mouseX, mouseY);
-      line(i,1000, mouseX, mouseY);
-      line(1000,i, mouseX, mouseY);
+function drawCirles(){
+  //TWO_PI is the circumference of a circle
+  //if 'angle'(which is the slowest moving circle)
+  //is smaller than TWO_PI then the circles are
+  //drawn
+    if(angle<TWO_PI){
+      //this part of the program insures the program
+      //has coordiantes
+      if(flag == "no"){
+        mouseCoordX = mouseX;
+        mouseCoordY = mouseY;
+        console.log(mouseCoordX);
+        console.log("yes");
+        flag = "yes";
+        }
+
+
+      else if(flag == "yes"){
+
+        stroke(180);
+        var negXCircleCoord = mouseCoordX - cos(angle2)*50;
+        var negYCircleCoord = mouseCoordY - sin(angle2)*50
+        var posXCircleCoord = mouseCoordX + cos(angle2)*50;
+        var posYCircleCoord = mouseCoordY + sin(angle2)*50;
+        var negXCircleCoord2 = mouseCoordX - cos(angle3)*100;
+        var negYCircleCoord2 = mouseCoordY - sin(angle3)*100;
+        var posXCircleCoord2 = mouseCoordX + cos(angle3)*100;
+        var posYCircleCoord2 = mouseCoordY + sin(angle3)*100;
+        var negXCircleCoord3 = mouseCoordX - cos(angle4)*150;
+        var negYCircleCoord3 = mouseCoordY - sin(angle4)*150;
+        var posXCircleCoord3 = mouseCoordX + cos(angle4)*150;
+        var posYCircleCoord3 = mouseCoordY + sin(angle4)*150;
+        var negXCircleCoord4 = mouseCoordX - cos(angle5)*200;
+        var negYCircleCoord4 = mouseCoordY - sin(angle5)*200;
+        var posXCircleCoord4 = mouseCoordX + cos(angle5)*200;
+        var posYCircleCoord4 = mouseCoordY + sin(angle5)*200;
+
+        background(255,255,255);
+        size = 50
+        fill(random(255),random(255),random(255),20);
+        ellipse(posXCircleCoord,posYCircleCoord,size,size);
+        ellipse(negXCircleCoord,negYCircleCoord,size,size);
+        ellipse(posXCircleCoord2,posYCircleCoord2,size,size);
+        ellipse(negXCircleCoord2,negYCircleCoord2,size,size);
+        ellipse(negXCircleCoord3,negYCircleCoord3,size,size);
+        ellipse(posXCircleCoord3,posYCircleCoord3,size,size);
+        ellipse(negXCircleCoord4,negYCircleCoord4,size,size);
+        ellipse(posXCircleCoord4,posYCircleCoord4,size,size);
+
+        fill(random(255),random(255),random(255),50);
+        ellipse(negXCircleCoord,negYCircleCoord,20,20);
+        ellipse(posXCircleCoord,posYCircleCoord,20,20);
+        ellipse(negXCircleCoord2,negYCircleCoord2,20,20);
+        ellipse(posXCircleCoord2,posYCircleCoord2,20,20);
+        ellipse(negXCircleCoord3,negYCircleCoord3,20,20);
+        ellipse(posXCircleCoord3,posYCircleCoord3,20,20);
+        ellipse(negXCircleCoord4,negYCircleCoord4,20,20);
+        ellipse(posXCircleCoord4,posYCircleCoord4,20,20);
+
+        fill(0,0,0,0);
+        ellipse(mouseCoordX,mouseCoordY, 50,50);
+        ellipse(mouseCoordX,mouseCoordY, 150,150);
+        ellipse(mouseCoordX,mouseCoordY, 250,250);
+        ellipse(mouseCoordX,mouseCoordY, 350,350);
+        ellipse(mouseCoordX,mouseCoordY, 450,450);
+
+        angle = angle + 0.1;
+        angle2 = angle2 + 0.2;
+        angle3 = angle3 + 0.3;
+        angle4 = angle4 + 0.4;
+        angle5 = angle5 + 0.5;
+
+
+        line(0,0, mouseCoordX , mouseCoordY);
+        line(width,height, mouseCoordX , mouseCoordY);
+        line(width,0, mouseCoordX, mouseCoordY);
+        line(0,height, mouseCoordX , mouseCoordY);
+        line(width/2,0, mouseCoordX , mouseCoordY,);
+        line(width/2,height, mouseCoordX , mouseCoordY,);
+        line(0,height/2, mouseCoordX , mouseCoordY,);
+        line(width,height/2, mouseCoordX , mouseCoordY,);
+
+        }
+
     }
-    else{
-      stroke(0,0,0);
-      line(0,i, mouseX, mouseY);
-      line(i,0, mouseX, mouseY);
-      line(i,1000, mouseX, mouseY);
-      line(1000,i, mouseX, mouseY);
-  }
-  }
-}
 
-function drawCentreCircle(){
-  fill(180);
-  noStroke();
-  ellipse(mouseX,mouseY,60,60);
-}
-
-function drawStillCirles(){
-  var xCoord = mouseX + 30;
-  var yCoord = mouseY + 30;
-  var xCoord2 = mouseX - 30;
-  var yCoord2 = mouseY - 30;
-  var size = 20;
-  var fade = 100;
-  noStroke();
-  for(x = 0; x<1000; x = x+30){
-    fade = fade -5;
-    fill(240,250,0,fade);
-    ellipse(xCoord + x, yCoord + x,size,size);
-    fill(250,200,0,fade);
-    ellipse(xCoord2 - x, yCoord2 - x,size,size);
-    size = size + 5;
-  }
-}
-
-
-function draw(){
-  //background(180);
-  drawLines();
-  drawCentreCircle();
-  //drawStillCirles();
-
-
-
+    else if(angle => TWO_PI){
+      background("clear");
+      angle = 0;
+      mouseCoordX = mouseX;
+      mouseCoordY = mouseY;
+    }
 }
