c917999533184b9825beca50c31d49fbebc1e3e1
Claire Stranack <44362052+cstranack@users.noreply.github.com>
2019-02-26 10:37:08 +0000

    added parallax, some links broken

--- PORTFOLIO/js/script.js
+++ PORTFOLIO/js/script.js
@@ -28,3 +28,30 @@ function addBounce(e){
 function removeBounce(e){
   this.classList.remove('animated', 'bounce');
 }
+
+
+
+
+
+
+var feed = new Instafeed({
+  get: 'user',
+  userId: '4021027637',
+  accessToken: '4021027637.3df9e66.49f1074a36f04d43a9f901f50892c3bc',
+  filter: function(image) {
+  var MAX_LENGTH = 40;
+
+  // here we create a property called "short_caption"
+  // on the image object, using the original caption
+  if (image.caption && image.caption.text) {
+    image.short_caption = image.caption.text.slice(0, MAX_LENGTH);
+  } else {
+    image.short_caption = "";
+  }
+
+  // ensure the filter doesn't reject any images
+  return true;
+},
+  template: '<div class="col-2"><img class="w-100" src={{image}}><hr><p class="likes"><i class="fas fa-heart"></i>{{likes}} <br> <i class="fas fa-magic"></i>{{model.filter}}<br><p class="instaCaption">{{model.short_caption}}...<a href="{{link}}" target="_blank">Read More</a></p></div>'
+});
+feed.run();
